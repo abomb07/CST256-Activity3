f179533ca6547973e73dc5354dfc41fb0000d439
Adam Bender <abend07@gmail.com>
2020-02-12 10:54:53 -0700

    ACID Transactions

---
+++ Database.php
@@ -0,0 +1,10 @@
+<?php
+
+class Database
+{
+    public static $dbservername = "localhost:3306";
+    public static $dbusername = "root";
+    public static $dbpassword = "root";
+    public static $dbname = "ica17";
+}
+
